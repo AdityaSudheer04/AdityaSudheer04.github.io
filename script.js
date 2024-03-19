7d1dc5b1b51dc31664cc71d79f7f2f6130bfac91
Zarvenik <adityasudheer.221ee102@nitk.edu.in>
2024-03-19 23:46:59 +0530

    Testing Voice

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.onload = () => {
         return Math.sqrt((pointA[0]-pointB[0])^2 + (pointA[1]-pointB[1])^2);
     }
 
-    async function tourGuidePosition(poiLat, poiLon, displacementMeters =10) {
+    async function tourGuidePosition(poiLat, poiLon, displacementMeters = 2) {
         let poiPosition = [poiLat, poiLon];
         let currentPosition = await getCurrentPosition();
     
